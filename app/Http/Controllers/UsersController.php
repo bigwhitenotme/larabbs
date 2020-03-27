da19b43f33e663447fa1b4f9221d56a2443245aa
haymax <1664667015@qq.com>
2020-03-27 14:51:08 +0800

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
-    //个人信息
+    //获取个人信息
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    // 编辑个人信息
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    //更新个人信息
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
